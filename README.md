bf300841ffac4d1654a57f648e766e68bf7a4185
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-09-12 20:02:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# About this repo
+
+This repo contains the samples according to the Udemy course "Apache Camel for beginners".
+
+## Relevant Links
+- [Udemy Course](<https://www.udemy.com/apache-camel-for-beginners-learn-by-coding-in-java/>)
+- [Source Code for this course](<https://github.com/dsaish3/TeachApacheCamel>)
+
+## Lessons
+1. Setup a route to copy files from source to destination folder
+2. Setup Logging
+3. How Camel transport the components
+4. Producers and Consumers
+5. Lab: Multiple Routes
---
+++ src/main/java/at/hochbichler/camel/CopyFilesCamel.java
@@ -0,0 +1,26 @@
+package at.hochbichler.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class CopyFilesCamel {
+    public static void main(String[] args) {
+        CamelContext context = new DefaultCamelContext();
+        try {
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("file:data/input?noop=true")
+                            .to("file:data/output");
+                }
+            });
+            context.start();
+            Thread.sleep(5000);
+            context.stop();
+        } catch (Exception e) {
+            System.out.println("Exception:  " +e);
+            e.printStackTrace();
+        }
+    }
+}
